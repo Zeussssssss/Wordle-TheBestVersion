3bef5fc14fb383f0f59f6b0d7fdca1be9a66f75e
Alankrit-Moses <113947973+Alankrit-Moses@users.noreply.github.com>
2022-11-25 20:24:58 -0700

    Main Menu

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,49 @@
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.FileNotFoundException;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class Main {
 
 	public static void main(String[] args) throws FileNotFoundException {
-		UI ui = new UI("dark");	
+		JFrame jf = new JFrame("WORDLE");
+		jf.setBounds(400,50,700,700);
+		jf.setVisible(true);
+		jf.setLayout(null);
+		JLabel title = new JLabel("WORDLE");
+		title.setFont(new Font("Arial",Font.BOLD,50));
+		jf.add(title);
+		title.setBounds(235, 100, 226, 100);
+		
+		JButton single = new JButton("New SinglePlayer");
+		single.setLayout(null);
+		single.setBounds(235,300,226,50);
+		JButton multi  = new JButton("New MultiPlayer");
+		multi.setLayout(null);
+		multi.setBounds(235,400,226,50);
+		jf.add(multi);
+		jf.add(single);
+		single.setVisible(true);
+		multi.setVisible(true);
+		single.addActionListener(new ActionListener()
+				{
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						try {
+							newGame();
+							jf.dispose();
+						} catch (FileNotFoundException e1) {}
+					}
+				});
+	}
+	
+	public static void newGame() throws FileNotFoundException
+	{
+		UI ui = new UI("light");	
 		WordleGame game = new WordleGame();
 		Controller controller = new Controller(ui, game);
 		ui.addController(controller);
