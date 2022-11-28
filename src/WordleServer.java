3112b770abac603dce2b3fe8a8eb257bf9a9f134
Aditya Kumar <67184967+Zeussssssss@users.noreply.github.com>
2022-11-28 00:28:50 -0700

    Server-side code completed

---
+++ src/WordleServer.java
@@ -0,0 +1,113 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+import java.util.concurrent.Executors;
+
+/**
+ * Server class
+ * @author Aditya
+ *
+ */
+public class WordleServer {
+	private static Game game = new Game();
+
+	public static void main(String[] args) throws Exception {
+		System.out.println(InetAddress.getLocalHost());
+
+		try (var listener = new ServerSocket(59896)) {
+			System.out.println("The Wordle server is running...");
+			var pool = Executors.newFixedThreadPool(2);
+			while (true) {
+				pool.execute(new ClientManager(listener.accept()));
+			}
+		}
+	}
+
+	/**
+	 * Manages every client that joins the server
+	 * @author Aditya
+	 *
+	 */
+	static class ClientManager implements Runnable {
+
+		private Player you;
+		private Socket socket;
+
+		public ClientManager(Socket socket) throws Exception {
+			this.socket = socket;
+			you = new Player();
+			if (game.getCurrentPlayer() == null) {
+				game.setPlayerOne(you);
+				you.getOutput().println("<< YOUR TURN STARTS");
+			}
+			else {
+				game.setPlayerTwo(you);
+				you.setTeamMate(game.getPlayerOne());
+				game.getPlayerOne().setTeamMate(you);
+			}
+			you.setInput(new Scanner(socket.getInputStream()));
+			you.setOutput(new PrintWriter(socket.getOutputStream(), true));			
+		}
+
+		@Override
+		public void run() {
+			try{
+				System.out.println("Connected: " + socket);
+				processCommands();
+			}catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				try {
+					socket.close();
+				} catch (IOException e) {
+				}
+				System.out.println("Closed: " + socket);
+			}
+		}
+
+		private synchronized void processCommands() {
+			while(you.getInput().hasNextLine()) {
+				if (you != game.getCurrentPlayer()) {
+					you.getOutput().println("<< REJECTED");
+					continue;
+				}
+				
+				var command = you.getInput().nextLine().replace("\n", "");
+				if(command.startsWith(">> ADD LETTER")) {
+					String letter = command.split(" ")[3];
+					you.getOutput().println("<< DONE");
+					you.getTeamMate().getOutput().println("<< ADD LETTER " + letter );
+				}
+				else if(command.equals(">> SUBMIT ATTEMPT")) { //Enter pressed
+					you.getOutput().println("<< DONE");
+					you.getTeamMate().getOutput().println("<< SUBMIT");
+					switchCurrentPlayer();
+					you.getOutput().println("<< YOUR TURN ENDS");
+					you.getTeamMate().getOutput().println("<< YOUR TURN STARTS");
+				}
+				else if(command.equals(">> DELETE")) {
+					you.getOutput().println("<< DONE");
+					you.getTeamMate().getOutput().println("<< DELETE");
+				}
+				else if(command.equals(">> RESTART")) { 
+					you.getOutput().println("<< DONE");
+					you.getTeamMate().getOutput().println("<< RESTART");
+				}
+				else if(command.equals(">> QUIT")) {
+					return; 
+				}
+			}
+		}
+		
+		private void switchCurrentPlayer() {
+			if (game.getCurrentPlayer() == game.getPlayerOne())
+				game.setCurrentPlayer(game.getPlayerTwo());
+			else
+				game.setCurrentPlayer(game.getPlayerOne());
+		}
+
+	}
+}
